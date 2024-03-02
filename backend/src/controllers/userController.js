94855723bc4618fa67f937ab2142139a72515b72
sriram kumar <sriramkumar@srirams-MacBook-Pro.local>
2024-03-02 17:06:25 -0500

    authorized

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -5,17 +5,40 @@ const expressAsyncHandler =require('express-async-handler');
 const jwt = require('jsonwebtoken');
 const bcrypt= require('bcryptjs');
 require('dotenv').config();
-
-const loginController = () => { };
+ 
+const loginController = expressAsyncHandler(async(req,res) => { 
+    // console.log(req.body);
+    const{username,email,password}= req.body;
+    if(!username||!email||!password){
+        res.sendStatus(400);
+        throw Error("All necessary fields have not been filled to sign in");
+    }
+    const user= await userModel.findOne({email});
+    if (!user){return res.status(401).json({message:"user not found"});}
+    const match = await bcrypt.compare(password,user.password);
+    console.log(match);
+    if(match){
+        console.log("logged in")
+        res.json({
+            _id :user._id,
+            username: user.username,
+            email:user.email,
+            isAdmin:user.isAdmin,
+            token:generateToken(user._id)
+        })
+    }else{
+        res.sendStatus(400);
+        throw new Error("unauthorized");
+}});
 
 const generateToken = (id)=>{
-    return jwt.sign(_id,process.env.ACCESS_TOKEN_SECRET,{expiresIn:"30d"});
+    return jwt.sign({id},process.env.ACCESS_TOKEN_SECRET,{expiresIn:"30d"});
 }
 
 const registerController = expressAsyncHandler(async (req, res) => {
     const{username, email, password} = req.body;
     //check for all fields
-    try{
+    
         if(!username||!email||!password){
             res.send(400);
             throw Error("All necessary fields have not been filled");
@@ -32,23 +55,21 @@ const registerController = expressAsyncHandler(async (req, res) => {
         }
         //creating a doc in db
         // const user=  await userModel.create({username, email, password});
-        
-            const hashedPassword = await bcrypt.hash(password,12);
+            const salt= await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(password,salt);
             const user= await userModel.create({username, email, password:hashedPassword});
             if(user){
-                res.status(201).json({
-                    _is:user._id,
+                res.json({
+                    id:user._id,
                     username : user.username,
                     email:user.email,
                     isAdmin:user.isAdmin,
                     token: generateToken(user._id)
 
                 })
+            }else{
+                console.log("registration error",err);
             }
-    }
-    catch(err){
-        console.log("something went wrong",err);
-    }
 });
 
 module.exports = {loginController, registerController};
