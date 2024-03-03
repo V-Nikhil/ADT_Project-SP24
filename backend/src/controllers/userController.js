6589eeeafbf7be2dffab5c0dbe43d022a5b3a814
sriram kumar <sriramkumar@srirams-MacBook-Pro.local>
2024-03-03 18:04:21 -0500

    messagemodel done

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -8,8 +8,8 @@ require('dotenv').config();
  
 const loginController = expressAsyncHandler(async(req,res) => { 
     // console.log(req.body);
-    const{username,email,password}= req.body;
-    if(!username||!email||!password){
+    const{name,email,password}= req.body;
+    if(!name||!email||!password){
         res.sendStatus(400);
         throw Error("All necessary fields have not been filled to sign in");
     }
@@ -21,7 +21,7 @@ const loginController = expressAsyncHandler(async(req,res) => {
         console.log("logged in")
         res.json({
             _id :user._id,
-            username: user.username,
+            name: user.name,
             email:user.email,
             isAdmin:user.isAdmin,
             token:generateToken(user._id)
@@ -36,11 +36,11 @@ const generateToken = (id)=>{
 }
 
 const registerController = expressAsyncHandler(async (req, res) => {
-    const{username, email, password} = req.body;
+    const{name, email, password} = req.body;
     //check for all fields
     
-        if(!username||!email||!password){
-            res.send(400);
+        if(!name||!email||!password){
+            res.sendStatus(400);
             throw Error("All necessary fields have not been filled");
         }
         // pre exisiting user
@@ -49,7 +49,7 @@ const registerController = expressAsyncHandler(async (req, res) => {
             throw Error("user Exists,please login");
         }
 
-        const userNameExists=await userModel.findOne({username});
+        const userNameExists=await userModel.findOne({name});
         if (userNameExists){
             throw Error("username already taken");
         }
@@ -57,11 +57,11 @@ const registerController = expressAsyncHandler(async (req, res) => {
         // const user=  await userModel.create({username, email, password});
             const salt= await bcrypt.genSalt(10)
             const hashedPassword = await bcrypt.hash(password,salt);
-            const user= await userModel.create({username, email, password:hashedPassword});
+            const user= await userModel.create({name, email, password:hashedPassword});
             if(user){
                 res.json({
                     id:user._id,
-                    username : user.username,
+                    name : user.name,
                     email:user.email,
                     isAdmin:user.isAdmin,
                     token: generateToken(user._id)
@@ -73,18 +73,12 @@ const registerController = expressAsyncHandler(async (req, res) => {
 });
 
 const fetchAllUsersController = expressAsyncHandler(async (req, res) => {
-    const keyword = req.query.search
-      ? {
-          $or: [
-            { name: { $regex: req.query.search, $options: "i" } },
-            { email: { $regex: req.query.search, $options: "i" } },
-          ],
-        }
-      : {};
-  
-    const users = await UserModel.find(keyword).find({
-      _id: { $ne: req.user._id },
-    });
+    const users = await UserModel.find({
+                $or: [ 
+                    { name: { $regex: req.query.search, $options: "i" } }, 
+                    { email: { $regex: req.query.search, $options: "i" } } 
+                ] 
+            }).find({ _id: { $ne: req.user._id } },);
     res.send(users);
   });
 
--- backend/src/models/MessageModel.js
+++ backend/src/models/MessageModel.js
@@ -4,7 +4,7 @@ const messageSchema = mongoose.Schema({
   content:{type:String,required: true},
   attachment:{ type: String},
   sender :{type:mongoose.Schema.Types.ObjectId,ref:'User',required: true},
-  reciever:{type: mongoose.Scheme.Types.ObjectID, ref:'User',required: true},
+  reciever:{type: mongoose.Schema.Types.ObjectID, ref:'User',required: true},
   chat:{type:mongoose.Schema.Types.ObjectId, ref: 'ChatRoom',required: true},
 },{timeStamp: true});
 
