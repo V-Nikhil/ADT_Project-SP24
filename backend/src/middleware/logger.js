6d3d6ef53960ca8f557244d84496062a8ae675f7
sriram kumar <sriramkumar@srirams-MacBook-Pro.local>
2024-03-04 02:29:31 -0500

    added all files

--- backend/src/middleware/logger.js
+++ backend/src/middleware/logger.js
@@ -1,7 +1,16 @@
-// function logger(req, res, next) {
-//     console.log(`${req.method} ${req.url}`);
-//     next(); // Move on to the next middleware or route handler
-//   }
+const notFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+  };
   
-//   module.exports = logger;
+  const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+      stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    });
+  };
   
+  module.exports = { notFound, errorHandler };
